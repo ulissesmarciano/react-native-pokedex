2a9a05e489d087f3ce27e8d43b11152543904b4d
Ulisses <ulissesmkt2104@gmail.com>
2025-01-07 22:52:10 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View 
+    style={{
+      marginTop: 34,
+      flex: 1,
+      backgroundColor: "yellow",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+      }}>
+      <Text>Ulisses Marciano</Text>
     </View>
   );
 }
