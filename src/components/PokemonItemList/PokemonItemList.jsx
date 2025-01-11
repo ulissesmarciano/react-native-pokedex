e8eeaf72a85d8cddf90198fc28e22a15445734ee
Ulisses <ulissesmkt2104@gmail.com>
2025-01-11 16:09:36 -0300

    Atualização e Criação: Exportação da folha de estilos para um novo arquivo

---
+++ src/components/PokemonItemList/PokemonItemList.jsx
@@ -0,0 +1,11 @@
+import { ItemContainer, ItemText } from './styles'
+
+const PokemonItemlist = () => {
+  return (
+    <ItemContainer>
+      <ItemText>teste</ItemText>
+    </ItemContainer>
+  );
+};
+
+export default PokemonItemlist; 
---
+++ src/components/PokemonItemList/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.View`
+  flex: 1;
+  height: 120px;
+  background-color: #f9c2ff;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+`;
+
+export const ItemText = styled.Text`
+  font-size: 16px;
+  color: #333;
+`;
+
+
