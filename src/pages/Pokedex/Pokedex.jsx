c075176c43b9507510fd422b7cccc9f5ab9ea0fa
Ulisses <ulissesmkt2104@gmail.com>
2025-01-11 18:08:40 -0300

    Atualzação: Criação da lista mocada para renderização dos cards de pokémons

--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -1,18 +1,60 @@
-import { View, StatusBar } from 'react-native';
-import styled from 'styled-components';
+import { View, StatusBar, FlatList } from 'react-native';
+import styled from 'styled-components/native';
 import Header from '../../components/PokedexHeader/PokedexHeader';
+import PokemonItemlist from '../../components/PokemonItemList/PokemonItemList';
+
+const NUM_COLUMNS = 2;
+
+const data = Array.from({ length: 13 }, (_, i) => `Item ${i + 1}`);
+
+const Pokedex = () => {
+  const formattedData = [...data];
+  while (formattedData.length % NUM_COLUMNS !== 0) {
+    formattedData.push(null);
+  }
+
+  return (
+    <Body>
+      <StatusBar barStyle="light-content" backgroundColor="#A40003" />
+      <Header />
+      <PokedexListContainer>
+        <FlatList
+          data={formattedData}
+          renderItem={({ item }) =>
+            item ? (
+              <PokemonItemWrapper>
+                <PokemonItemlist title={item} />
+              </PokemonItemWrapper>
+            ) : (
+              <PokemonItemWrapper />
+            )
+          }
+          numColumns={NUM_COLUMNS}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </PokedexListContainer>
+    </Body>
+  );
+};
 
 const Body = styled(View)`
-    background-color: #FF010B;
-    height: 100%;
-`
-const Pokedex = () => {
-    return (
-        <Body>
-            <StatusBar barStyle="light-content" backgroundColor="#A40003" />
-            <Header />
-        </Body>
-    );
-};
+  background-color: #FF010B;
+  height: 100%;
+`;
+
+const PokedexListContainer = styled(View)`
+  margin: 14px;
+  flex: 1;
+  background-color: #363636;
+  border-width: 1px;
+  border-color: #000;
+  border-radius: 10px;
+`;
+
+const PokemonItemWrapper = styled(View)`
+  flex: 1;
+  margin: 6px;
+  max-width: 50%;
+`;
 
 export default Pokedex;
