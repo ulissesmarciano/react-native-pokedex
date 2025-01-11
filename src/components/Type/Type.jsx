55a65bd4879c7f83022fc5699070250416a7c734
Ulisses <ulissesmkt2104@gmail.com>
2025-01-11 18:46:51 -0300

    Criação: Módulo para renderização dos tipos de pokémon

---
+++ src/components/Type/Type.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Type = () => {
+  return (
+    <Container>
+      <TypeName>electric</TypeName>
+    </Container>
+  );
+};
+
+
+const Container = styled.View`
+    padding: 2px 8px;
+    align-items: center;
+    justify-content: center;
+    background-color: green;
+    border-radius: 10px;
+`
+
+const TypeName = styled.Text`
+    color: #333;
+    text-transform: capitalize;
+    font-weight: 600;
+`
+
+export default Type;
