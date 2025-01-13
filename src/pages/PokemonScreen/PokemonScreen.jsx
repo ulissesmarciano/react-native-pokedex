c250fcee78dddb73fc7598c22124b21af3384365
Ulisses <ulissesmkt2104@gmail.com>
2025-01-13 12:13:46 -0300

    Criação: página para renderização do pokemon e suas especificidades

---
+++ src/pages/PokemonScreen/PokemonScreen.jsx
@@ -0,0 +1,25 @@
+import { View, Button, StyleSheet } from 'react-native';
+
+const PokemonScreen = () => {
+  return (
+    <View>
+      <Button
+        title="Go back to Home"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 24,
+    },
+  });
+
+export default PokemonScreen;
