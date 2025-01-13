c4696c0b491969e566b716062ead6c8f440cf736
Ulisses <ulissesmkt2104@gmail.com>
2025-01-13 14:23:56 -0300

    Atualização: Criando rotas para navegação entre duas páginas.

--- App.jsx
+++ App.jsx
@@ -1,10 +1,28 @@
-import { View } from "react-native";
-import Pokedex from "./src/pages/Pokedex/Pokedex";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import PokedexScreen from "./src/pages/PokedexScreen/PokedexScreen";
+import PokemonScreen from "./src/pages/PokemonScreen/PokemonScreen";
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
-    <View >
-      <Pokedex />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Pokedex">
+        <Stack.Screen 
+          name="Pokedex" 
+          component={PokedexScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Pokemon" 
+          component={PokemonScreen} 
+          options={{ title: 'Pokemon Page' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
