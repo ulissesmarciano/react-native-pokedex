d2082176f2ffe83c88bb82cfb0690b7718d28c5b
Ulisses <ulissesmkt2104@gmail.com>
2025-01-16 12:37:09 -0300

    Atualização: Parametrização do SearchInput

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,13 +1,15 @@
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Input } from './styles';
 
-const SearchInput = () => {
+const SearchInput = ({ onChangeText, value }) => {
   return (
     <Container>
       <Icon name="search" size={20} color="#ffffffab" />
       <Input
+        onChangeText={onChangeText}
         placeholder="Buscar"
         placeholderTextColor="#ffffffab"
+        value={value}
       />
     </Container>
   );
