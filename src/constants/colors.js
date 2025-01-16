6a7c5d5f27691b2ce40810f9fc5ef43bd5f918f0
Ulisses <ulissesmkt2104@gmail.com>
2025-01-15 23:30:19 -0300

    Criação: Diretório de constantes para o projeto

---
+++ src/constants/colors.js
@@ -0,0 +1,55 @@
+export const pokemonTypeStyles = {
+    normal: '#a6a877',
+    
+
+    grass: '#77c850',
+    
+
+    fire: '#ee7f30',
+    
+
+    water: '#678fee',
+    
+
+    electric: '#f7cf2e',
+    
+
+    ice: '#98d5d7',
+    
+
+    ground: '#dfbf69',
+    
+
+    flying: '#a98ff0',
+    
+
+    poison: '#a040a0',
+    
+
+    fighting: '#bf3029',
+    
+
+    psychic: '#f65687',
+    
+
+    dark: '#725847',
+    
+
+    rock: '#b8a137',
+    
+
+    bug: '#a8b720',
+    
+
+    ghost: '#6e5896',
+    
+    
+    steel: '#b9b7cf',
+    
+
+    dragon: '#6f38f6',
+    
+
+    fairy: '#f9aec7',
+    
+};
