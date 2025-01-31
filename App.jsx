0b850694e594843cbb7be93d759b6111153f2857
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-30 21:32:29 -0300

    Atualização: Estilização do header no stack screen pokemon

--- App.jsx
+++ App.jsx
@@ -1,28 +1,26 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import PokedexScreen from "./src/pages/PokedexScreen/PokedexScreen";
 import PokemonScreen from "./src/pages/PokemonScreen/PokemonScreen";
 
 const Stack = createNativeStackNavigator();
 
-
-
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Pokedex">
-        <Stack.Screen 
-          name="Pokedex" 
-          component={PokedexScreen} 
-          options={{ headerShown: false }} 
+        <Stack.Screen
+          name="Pokedex"
+          component={PokedexScreen}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="Pokemon" 
-          component={PokemonScreen} 
-          options={{ title: 'Pokemon Page' }} 
+        <Stack.Screen
+          name="Pokemon"
+          component={PokemonScreen}
+          options={{ title: "", headerTransparent: true }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
