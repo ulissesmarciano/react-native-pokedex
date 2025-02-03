817b8ed61dbb8fbf898fdb6f3ca7e2ed0b1ec32d
Ulisses <ulissesmarcianodev@gmail.com>
2025-02-02 21:51:00 -0300

    Atualização: Adicionando variante e tornando o compomente dinâmico para renderização na página.

--- src/components/ProgressBar/Progressbar.jsx
+++ src/components/ProgressBar/Progressbar.jsx
@@ -1,52 +1,49 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
+import { progressLimitStyle } from "../../constants/progressStyle";
 
-const ProgressBar = ({ progress, title }) => {
-    return (
-        <Container>
-            <ProgressTitle>{title}</ProgressTitle>
-            <ProgressBarSpace>
-                <Progress>
-                    <ProgressData>40/100</ProgressData>
-                </Progress>
-            </ProgressBarSpace>
-        </Container>
-    );
+const ProgressBar = ({ progress, title, progressBGColor, limit, variant }) => {
+  return (
+    <Container>
+      <ProgressTitle>{title}</ProgressTitle>
+      <ProgressBarSpace>
+        <Progress variant={variant} progressBGColor={progressBGColor} progress={progress}>
+          <ProgressData>
+            {progress}/{limit}
+          </ProgressData>
+        </Progress>
+      </ProgressBarSpace>
+    </Container>
+  );
 };
 
 const Container = styled.View`
-    flex-direction: row;
-    align-items: center;
-    gap: 10px;
-    justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  justify-content: space-between;
 `;
 
 const ProgressTitle = styled.Text`
-    color: #DBDBDB;
-    text-transform: uppercase;
+  color: #dbdbdb;
+  text-transform: uppercase;
 `;
 
 const ProgressBarSpace = styled.View`
-    height: 20px;
-    max-width: 90%;
-    flex: 1;
-    background-color: #fff;
-    border-radius: 10px;
-    overflow: hidden;
+  height: 20px;
+  max-width: 90%;
+  flex: 1;
+  background-color: #fff;
+  border-radius: 10px;
+  overflow: hidden;
 `;
 
 const Progress = styled.View`
-    padding-right: 8px;
-    height: 100%;
-    width: 40%;
-    background-color: #76c7c0;
-    border-radius: 10px;
-    align-items: flex-end;
+  ${({variant}) => progressLimitStyle[variant] || progressLimitStyle.primary}
 `;
 
 const ProgressData = styled.Text`
-    color: #fff;
+  color: #fff;
 `;
-
 
 export default ProgressBar;
