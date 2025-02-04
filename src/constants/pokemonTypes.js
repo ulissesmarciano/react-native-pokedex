7db0d40aa1df00d2f663e649ce1e77fd79ebc173
Ulisses <ulissesmarcianodev@gmail.com>
2025-02-03 23:28:58 -0300

    Criação: Componente de constantes para renderizar os tipos no sidebar.

---
+++ src/constants/pokemonTypes.js
@@ -0,0 +1,91 @@
+export const pokemonTypes = [
+  {
+    name: "normal",
+    color: "#a6a877",
+  },
+
+  {
+    name: "grass",
+    color: "#77c850",
+  },
+
+  {
+    name: "fire",
+    color: "#ee7f30",
+  },
+
+  {
+    name: "water",
+    color: "#678fee",
+  },
+
+  {
+    name: "electric",
+    color: "#f7cf2e",
+  },
+
+  {
+    name: "ice",
+    color: "#98d5d7",
+  },
+
+  {
+    name: "ground",
+    color: "#dfbf69",
+  },
+
+  {
+    name: "flying",
+    color: "#a98ff0",
+  },
+
+  {
+    name: "poison",
+    color: "#a040a0",
+  },
+
+  {
+    name: "fighting",
+    color: "#bf3029",
+  },
+
+  {
+    name: "psychic",
+    color: "#f65687",
+  },
+
+  {
+    name: "dark",
+    color: "#725847",
+  },
+
+  {
+    name: "rock",
+    color: "#b8a137",
+  },
+
+  {
+    name: "bug",
+    color: "#a8b720",
+  },
+
+  {
+    name: "ghost",
+    color: "#6e5896",
+  },
+
+  {
+    name: "steel",
+    color: "#b9b7cf",
+  },
+
+  {
+    name: "dragon",
+    color: "#6f38f6",
+  },
+
+  {
+    name: "fairy",
+    color: "#f9aec7",
+  },
+];
